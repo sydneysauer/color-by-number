645bf43b89b8cc08860337831a84a9302cdcef11
sydney.sauer <sydney.sauer@outlook.com>
2020-05-22 00:22:45 -0400

    Implemented form functionality for submit and refresh

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
+//TODO turn blocks white on restart + can we change the selection color? maybe add above grid (branch!!!!!)
+
 var boxes = document.querySelectorAll(".section");
 
-// Make boxes clickable
+// Box panel functions
 for (const box of boxes) {   
     box.addEventListener("click", function selectBox(e) {
         console.log(e.target);
@@ -8,6 +10,47 @@ for (const box of boxes) {
     });  
 }
 
+// Form functions
+const saveButton = document.querySelector("#save");
+saveButton.addEventListener("click", function saveData() {
+    const nameField = document.getElementById("name");
+    const colorField = document.getElementById("color");
+    var name = nameField.value;
+    var color = colorField.value;
+    var selectedDivs = document.querySelectorAll(".selected");
+    selectedDivs.forEach(function setProperties(item) {
+        if (!item.classList.contains("submitted")) {
+            item.style.background = color;
+            //item.innerHTML = "<span class=\"highlight\">" + name + "</span>";
+            var nameText = document.createElement("P");
+            nameText.innerHTML = "<span class=\"highlight\">" + name + "</span>";
+            item.appendChild(nameText);
+            item.classList.add("submitted");         
+        }
+        item.classList.remove("selected");
+    });
+    console.log(selectedDivs);
+
+
+    // Reset fields to default
+    nameField.value = "";
+    colorField.value = "#000000";    
+});
+
+const restartButton = document.querySelector('#restart');
+restartButton.addEventListener("click", function deselectAll() {
+    window.location.reload();
+    /*
+    for (const box of boxes) {
+        box.classList.remove("selected");
+        box.classList.remove("submitted");
+        box.innerHTML = "";   
+        boxes.forEach(function(item) {
+            item.style.background = "white";
+        }); 
+    }
+    */
+});
 
 
     
